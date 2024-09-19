4cd168859cfd53d5f71b301aaf375975c19e0b19
EnesAkyuz <eakyuz0681@gmail.com>
2024-09-19 20:53:24 -0300

    Emptied Style

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
     
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on http://localhost:${PORT}`)
